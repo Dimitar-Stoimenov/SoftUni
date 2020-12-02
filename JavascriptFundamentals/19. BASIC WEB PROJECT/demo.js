911e6a33ffc834f39b3c8d177fb9012b6daa38b5
Dimitar Stoimenov <dstoimen0@gmail.com>
2020-12-02 23:44:34 +0200

    commit

--- JavascriptFundamentals/19. BASIC WEB PROJECT/demo.js
+++ JavascriptFundamentals/19. BASIC WEB PROJECT/demo.js
@@ -1,4 +1,9 @@
 const data = require('./someModule');
+const fs = require('fs');
+const express = require('express');
 
 console.log(data.name);
 console.log(data.secondName);
+
+let content = fs.readFileSync('c:/Users/dstoi/Desktop/GitHub Repository/SoftUni/JavascriptFundamentals/19. BASIC WEB PROJECT/text.js', { encoding: 'utf-8' });
+console.log(content);
