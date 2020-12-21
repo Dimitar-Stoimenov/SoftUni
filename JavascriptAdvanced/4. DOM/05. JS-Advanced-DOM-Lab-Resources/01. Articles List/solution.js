17afd6c706d837f9c37d11820b91fdc663a578ff
Dimitar Stoimenov <dstoimen0@gmail.com>
2020-12-21 19:40:59 +0200

    updated solutions

--- JavascriptAdvanced/4. DOM/05. JS-Advanced-DOM-Lab-Resources/01. Articles List/solution.js
+++ JavascriptAdvanced/4. DOM/05. JS-Advanced-DOM-Lab-Resources/01. Articles List/solution.js
@@ -2,18 +2,21 @@ function createArticle() {
 	let titleElement = document.getElementById('createTitle');
 	let contentElement = document.getElementById('createContent');
 
-	let headingElement = document.createElement('h3');
-	headingElement.innerHTML = titleElement.value;
-	titleElement.value = ''
-
-	let articleBodyElement = document.createElement('p')
-	articleBodyElement.innerHTML = contentElement.value;
-	contentElement.value = ''
-
-	let articleElement = document.createElement('article');
-	articleElement.appendChild(headingElement);
-	articleElement.appendChild(articleBodyElement);
-
-	let articleSectionElement = document.getElementById('articles');
-	articleSectionElement.appendChild(articleElement)
+	if (titleElement.value != '' && contentElement.value != '') {
+		let headingElement = document.createElement('h3');
+		headingElement.innerHTML = titleElement.value;
+		
+		let articleBodyElement = document.createElement('p')
+		articleBodyElement.innerHTML = contentElement.value;
+		
+		let articleElement = document.createElement('article');
+		articleElement.appendChild(headingElement);
+		articleElement.appendChild(articleBodyElement);
+		
+		let articleSectionElement = document.getElementById('articles');
+		articleSectionElement.appendChild(articleElement)
+		
+		titleElement.value = ''
+		contentElement.value = ''
+	}
 }
--- JavascriptAdvanced/4. DOM/05. JS-Advanced-DOM-Lab-Resources/02. Format the Text/solution.js
+++ JavascriptAdvanced/4. DOM/05. JS-Advanced-DOM-Lab-Resources/02. Format the Text/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO...
+    let text = document.getElementById('input').innerText
+    let sentences = text.split('.').filter(x => x !== '')
+
+    let paragraphs = []
+    let counter = 0
+
+    for (let i = 0; i < sentences.length; i += 3) {
+        paragraphs[counter] = []
+        paragraphs[counter].push(sentences[i])
+
+        if (i + 1) {
+            paragraphs[counter].push(sentences[i + 1])
+        }
+
+        if (i + 2) {
+            paragraphs[counter].push(sentences[i + 2])
+        }
+
+        counter++
+    }
+
+    for (const array of paragraphs) {
+        let paragraph = document.createElement('p')
+        paragraph.innerText = array.join('.') + '.'
+        let output = document.getElementById("output")
+        output.appendChild(paragraph)
+    }
 }
