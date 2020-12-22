57f60482b0497f1828e6c313dd12c6da8a9ed402
Dimitar Stoimenov <dstoimen0@gmail.com>
2020-12-22 17:36:53 +0200

    added solution to 4. Order The Names

--- JavascriptAdvanced/4. DOM - EXERCISE/05. JS-Advanced-DOM-Exercise-Resources/05. JS-Advanced-DOM-Exercise-Resources/04. Order-the-Names/solution.js
+++ JavascriptAdvanced/4. DOM - EXERCISE/05. JS-Advanced-DOM-Exercise-Resources/05. JS-Advanced-DOM-Exercise-Resources/04. Order-the-Names/solution.js
@@ -1,4 +1,36 @@
 function solve() {
+    let inputName = document.getElementsByTagName('input')[0]
+    let btn = document.getElementsByTagName('button')[0]
+    let database = {}
+    counter = 0
 
-    //TODO...
+    for (let ascii = 65; ascii < 91; ascii++) {
+        let symbol = String.fromCharCode(ascii)
+        database[symbol] = counter++
+    }
+
+    btn.addEventListener('click', () => {
+        let ol = document.getElementsByTagName('ol')[0].children
+        let firstChar = inputName.value[0].toUpperCase()
+        let name = ''
+
+        for (let i = 0; i < inputName.value.length; i++) {
+            if (i === 0) {
+                name = inputName.value[0].toUpperCase()
+            } else if (inputName.value[i] === ' ') {
+                name += inputName.value[i++].toLowerCase()
+                name += inputName.value[i].toUpperCase()
+            } else {
+                name += inputName.value[i].toLowerCase()
+            }
+        }
+
+        if (ol[database[firstChar]].innerHTML.length === 0) {
+            ol[database[firstChar]].innerHTML = name
+        } else {
+            ol[database[firstChar]].innerHTML += ', ' + name
+        }
+
+        inputName.value = ''
+    })
 }
