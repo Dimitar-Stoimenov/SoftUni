18ea91c4f6e78db5957086b6d10e6007a2d385ac
Dimitar Stoimenov <dstoimen0@gmail.com>
2020-12-23 17:19:27 +0200

    commit calculator solution

--- JavascriptAdvanced/4. DOM - EXERCISE/05. JS-Advanced-DOM-Exercise-Resources/05. JS-Advanced-DOM-Exercise-Resources/06. Numpad-Calculator/solution.js
+++ JavascriptAdvanced/4. DOM - EXERCISE/05. JS-Advanced-DOM-Exercise-Resources/05. JS-Advanced-DOM-Exercise-Resources/06. Numpad-Calculator/solution.js
@@ -1,4 +1,54 @@
 function solve() {
+    let expression = document.getElementById('expressionOutput')
+    let resultBox = document.getElementById('resultOutput')
 
-    //TODO...
+    document.querySelector('.clear').addEventListener('click', clear)
+    document.querySelector('.keys').addEventListener('click', symbolClicked)
+
+    function clear() {
+        expression.textContent = ''
+        resultBox.textContent = ''
+    }
+
+    function symbolClicked(event) {
+        let buttonPressed = event.target.value
+
+        switch (buttonPressed) {
+            case '/':
+            case '*':
+            case '+':
+            case '-':
+                expression.textContent += ` ${buttonPressed} `
+                break;
+            case '=':
+                let [firstNum, operand, secondNum] = expression.textContent.split(' ')
+                let result = 0
+
+                if (!secondNum || !operand) {
+                    resultBox.textContent = 'NaN'
+                    break;
+                }
+
+                switch (operand) {
+                    case '/':
+                        result = Number(firstNum) / Number(secondNum)
+                        break;
+                    case '*':
+                        result = Number(firstNum) * Number(secondNum)
+                        break;
+                    case '+':
+                        result = Number(firstNum) + Number(secondNum)
+                        break;
+                    case '-':
+                        result = Number(firstNum) - Number(secondNum)
+                        break;
+                }
+
+                resultBox.textContent = result
+                break;
+            default:
+                expression.textContent += buttonPressed
+                break;
+        }
+    }
 }
