052c4fa2418607ab69667e39d6ce574c5f187dbf
Dimitar Stoimenov <dstoimen0@gmail.com>
2020-12-23 22:20:01 +0200

    push solution number-convertor

--- JavascriptAdvanced/4. DOM - EXERCISE/05. JS-Advanced-DOM-Exercise-Resources/05. JS-Advanced-DOM-Exercise-Resources/07. Number-Convertor/solution.js
+++ JavascriptAdvanced/4. DOM - EXERCISE/05. JS-Advanced-DOM-Exercise-Resources/05. JS-Advanced-DOM-Exercise-Resources/07. Number-Convertor/solution.js
@@ -1,4 +1,41 @@
 function solve() {
+    let numberBox = document.getElementById('input')
+    let btn = document.getElementById('container').lastElementChild
+    btn.addEventListener('click', convert)
+    let selectMenu = document.getElementById('selectMenuTo')
 
-    //TODO...
+    let binaryOption = document.createElement('option')
+    binaryOption.value = 'binary'
+    binaryOption.textContent = 'Binary'
+    selectMenu.appendChild(binaryOption)
+
+    let hexadecimal = document.createElement('option')
+    hexadecimal.value = 'hexadecimal'
+    hexadecimal.textContent = 'Hexadecimal'
+    selectMenu.appendChild(hexadecimal)
+
+    let resultBox = document.getElementById('result')
+
+    function convert() {
+        let numberToBeConverted = numberBox.value
+
+        switch (selectMenu.value) {
+            case 'binary':
+                resultBox.value = binaryConvertion(numberToBeConverted)
+                break;
+            case 'hexadecimal':
+                resultBox.value = hexadecimalConvertion(numberToBeConverted)
+                break;
+        }
+    }
+
+    function binaryConvertion(number) {
+        let result = Number(number).toString(2)
+        return result
+    }
+
+    function hexadecimalConvertion(number) {
+        let result = Number(number).toString(16).toUpperCase()
+        return result
+    }
 }
