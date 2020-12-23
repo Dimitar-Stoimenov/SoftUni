6d579f43368bf5ccf1db2225a22f15efd73f798f
Dimitar Stoimenov <dstoimen0@gmail.com>
2020-12-23 15:01:56 +0200

    added chatroom solution

--- JavascriptAdvanced/4. DOM - EXERCISE/05. JS-Advanced-DOM-Exercise-Resources/05. JS-Advanced-DOM-Exercise-Resources/05. Chat-Room/solution.js
+++ JavascriptAdvanced/4. DOM - EXERCISE/05. JS-Advanced-DOM-Exercise-Resources/05. JS-Advanced-DOM-Exercise-Resources/05. Chat-Room/solution.js
@@ -1,5 +1,16 @@
 function solve() {
-   //TODO...
+   let btn = document.getElementById('send')
+
+   btn.addEventListener('click', () => {
+      let messageBox = document.getElementById('chat_input')
+      let message = messageBox.value
+
+      let parentElement = document.getElementById('chat_messages')
+      let newMessage = document.createElement('div')
+      parentElement.appendChild(newMessage)
+      newMessage.textContent = message
+      newMessage.className = 'message my-message'
+
+      messageBox.value = ''
+   })
 }
-
-
