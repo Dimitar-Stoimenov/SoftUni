5208bcae38391f217322864a814d2018c736a8fe
Dimitar Stoimenov <dstoimen0@gmail.com>
2020-12-30 23:08:16 +0200

    push a solution

--- JavascriptAdvanced/5. DOM Manipulations/03. Delete-From-Table/app - from lab.js
+++ JavascriptAdvanced/5. DOM Manipulations/03. Delete-From-Table/app - from lab.js
@@ -5,21 +5,18 @@ function deleteByEmail() {
     let resultDiv = document.getElementById('result');
 
     let customerEmails = document.querySelectorAll('#customers td:nth-of-type(2)');
-    let customerEmailsValueArray = []
-    
-    for (const customerElement of customerEmails) {
-        customerEmailsValueArray.push(customerElement.textContent)
-    }
-    
+    let isDeleted = false
+
     for (const customerElement of customerEmails) {
         if (enteredEmail === customerElement.textContent) {
             customerElement.parentElement.remove()
             resultDiv.textContent = ''
-            break;
-        } else if (!customerEmailsValueArray.includes(enteredEmail)) {
-            resultDiv.textContent = 'Not found.'
-            break;
+            isDeleted = true
         }
+    }
+
+    if (!isDeleted) {
+        resultDiv.textContent = 'Not found.'
     }
 
     inputElement.value = ''
--- JavascriptAdvanced/5. DOM Manipulations/04. Stopwatch/stopwatch.js
+++ JavascriptAdvanced/5. DOM Manipulations/04. Stopwatch/stopwatch.js
@@ -1,3 +1,48 @@
 function stopwatch() {
-    console.log('TODO:...');
+    let timerElement = document.getElementById('time');
+
+    let startBtnElement = document.getElementById('startBtn');
+    let stopBtnElement = document.getElementById('stopBtn');
+    let interval;
+    //тук сет-вам променливата предварително, за да може да се използва извън скоупа на старт бутона
+
+    startBtnElement.addEventListener('click', function (e) {
+        timerElement.textContent = '00:00';
+        //тук зануляваме таймера, само веднъж - това не е в сетИнтервал функцията;
+        startBtnElement.setAttribute('disabled', true);
+        //тук може и с e.currentTarget.setAttribute('disabled', true)
+        stopBtnElement.removeAttribute('disabled');
+
+        interval = setInterval(function () {
+            let currentTime = timerElement.textContent;
+            let timeArray = currentTime.split(':');
+            minutes = Number(timeArray[0]);
+            seconds = Number(timeArray[1]);-
+
+            seconds++;
+
+            if (seconds > 59) {
+                seconds = '00';
+                minutes++;
+            } else if (seconds < 10) {
+                seconds = '0' + seconds;
+            }
+
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+            // вместо това, можеше да използвам .padStart(2, '0') - долу в output-a; падстарт работи върху стринг:
+            // timerElement.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+
+            timerElement.textContent = `${minutes}:${seconds}`;
+        }, 1000)
+
+    });
+
+    stopBtnElement.addEventListener('click', function (e) {
+        stopBtnElement.setAttribute('disabled', true);
+        startBtnElement.removeAttribute('disabled')
+
+        clearInterval(interval)
+    });
 }
