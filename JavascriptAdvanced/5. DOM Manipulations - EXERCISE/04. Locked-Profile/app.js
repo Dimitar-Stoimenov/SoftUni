e756aaa55cd87a5880b8724b198354b79e70e39d
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-01-02 18:49:07 +0200

    added one more solution

--- JavascriptAdvanced/5. DOM Manipulations - EXERCISE/04. Locked-Profile/app.js
+++ JavascriptAdvanced/5. DOM Manipulations - EXERCISE/04. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let btns = document.querySelectorAll('button')
+    btns = Array.from(btns)
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            let parentEl = btn.parentElement
+            let unlock = parentEl.getElementsByTagName('input')[1]
+            let hiddenDiv = parentEl.getElementsByTagName('div')[0]
+
+            if (unlock.checked) {
+                if (btn.textContent === 'Hide it') {
+                    btn.textContent = 'Show more'
+                    hiddenDiv.style.display = 'none'
+                } else {
+                    btn.textContent = 'Hide it'
+                    hiddenDiv.style.display = 'block'
+                }
+            }
+        })
+    })
 }
