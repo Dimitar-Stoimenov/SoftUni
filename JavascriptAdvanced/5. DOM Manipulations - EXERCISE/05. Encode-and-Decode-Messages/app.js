ababc5e00aad153adfa3938927758fc307b294c5
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-01-03 19:39:43 +0200

    adding more solutions

--- JavascriptAdvanced/5. DOM Manipulations - EXERCISE/05. Encode-and-Decode-Messages/app.js
+++ JavascriptAdvanced/5. DOM Manipulations - EXERCISE/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,34 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let mainDivElement = document.getElementById('main')
+
+    let encodeButton = mainDivElement.children[0].lastElementChild
+    let encodeTextElement = encodeButton.previousElementSibling
+
+    let decodeButton = mainDivElement.children[1].lastElementChild
+    let decodeTextElement = decodeButton.previousElementSibling
+
+    encodeButton.addEventListener('click', () => {
+        let text = encodeTextElement.value
+        let output = ''
+
+        for (let i = 0; i < text.length; i++) {
+            let char = text[i]
+            output += String.fromCharCode((char.charCodeAt(0)) + 1)
+        }
+
+        encodeTextElement.value = ''
+        decodeTextElement.value = output
+    })
+
+    decodeButton.addEventListener('click', () => {
+        let text = decodeTextElement.value
+        let output = ''
+
+        for (let i = 0; i < text.length; i++) {
+            let char = text[i]
+            output += String.fromCharCode((char.charCodeAt(0)) - 1)
+        }
+
+        decodeTextElement.value = output
+    })
 }
--- JavascriptAdvanced/5. DOM Manipulations - EXERCISE/06. Table-Search-Engine/solution.js
+++ JavascriptAdvanced/5. DOM Manipulations - EXERCISE/06. Table-Search-Engine/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-   //TODO...
+   let searchBtn = document.getElementById('searchBtn')
+   let searchTextElement = document.getElementById('searchField')
+
+   searchBtn.addEventListener('click', search)
+
+   function search() {
+
+      Array.from(document.querySelectorAll('tbody tr')).forEach(row => {
+         row.classList.remove('select')
+
+         if (row.textContent.includes(searchTextElement.value)) {
+            row.classList.add('select')
+         }
+      })
+
+      searchTextElement.value = ''
+   }
 }
