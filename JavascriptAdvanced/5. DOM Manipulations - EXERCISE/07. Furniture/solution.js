756dd88dddf324c09997bc2581bb6a59ba50f714
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-01-03 22:32:48 +0200

    added more solutions

--- JavascriptAdvanced/5. DOM Manipulations - EXERCISE/07. Furniture/solution.js
+++ JavascriptAdvanced/5. DOM Manipulations - EXERCISE/07. Furniture/solution.js
@@ -1,4 +1,70 @@
 function solve() {
+    let generateBtn = document.querySelectorAll('button')[0]
+    let generateTextAreaElement = document.querySelectorAll('textarea')[0]
+    let buyBtn = document.querySelectorAll('button')[1]
+    let tbodyElement = document.querySelector('tbody')
+    let resultTextAreaElement = document.querySelectorAll('textarea')[1]
 
-  //TODO...
+    generateBtn.addEventListener('click', () => {
+        let collection = JSON.parse(generateTextAreaElement.value)
+
+        for (const obj of collection) {
+            let newTr = document.createElement('tr')
+
+            let htmlTd = document.createElement('td')
+            let img = document.createElement('img')
+            img.src = obj.img
+            htmlTd.appendChild(img)
+            newTr.appendChild(htmlTd)
+
+            let nameTd = document.createElement('td')
+            let nameP = document.createElement('p')
+            nameP.textContent = obj.name
+            nameTd.appendChild(nameP)
+            newTr.appendChild(nameTd)
+
+            let priceTd = document.createElement('td')
+            let priceP = document.createElement('p')
+            priceP.textContent = obj.price
+            priceTd.appendChild(priceP)
+            newTr.appendChild(priceTd)
+
+            let decFactor = document.createElement('td')
+            let decP = document.createElement('p')
+            decP.textContent = obj.decFactor
+            decFactor.appendChild(decP)
+            newTr.appendChild(decFactor)
+
+            let markTd = document.createElement('td')
+            let markInputEl = document.createElement('input')
+            markInputEl.type = 'checkbox'
+            markTd.appendChild(markInputEl)
+            newTr.appendChild(markTd)
+
+            tbodyElement.appendChild(newTr)
+        }
+    })
+
+    buyBtn.addEventListener('click', () => {
+        let furnitureNameArray = []
+        let price = 0
+        let furnitureCounter = 0
+        let totalDecFactor = 0
+
+        Array.from(document.querySelectorAll('tbody tr')).forEach(row => {
+            if (row.lastElementChild.firstElementChild.checked) {
+                let furniturePropertiesArray = row.querySelectorAll('td')
+                furnitureNameArray.push(furniturePropertiesArray[1].firstElementChild.textContent)
+                price += Number(furniturePropertiesArray[2].firstElementChild.textContent)
+                furnitureCounter++
+                totalDecFactor += Number(furniturePropertiesArray[3].firstElementChild.textContent)
+            }
+        })
+
+        let avgDecFactor = totalDecFactor / furnitureCounter
+
+        let result = `Bought furniture: ${furnitureNameArray.join(', ')}\nTotal price: ${price.toFixed(2)}\nAverage decoration factor: ${avgDecFactor}`
+
+        resultTextAreaElement.textContent = result
+    })
 }
--- JavascriptAdvanced/5. DOM Manipulations - EXERCISE/08. Cards/solution.js
+++ JavascriptAdvanced/5. DOM Manipulations - EXERCISE/08. Cards/solution.js
@@ -1,3 +1,63 @@
 function solve() {
-   // TODO...
+   let firstPlayerCardsCollection = document.getElementById('player1Div')
+   let secondPlayerCardsCollection = document.getElementById('player2Div')
+   let resultDivElement = document.getElementById('result')
+   let firstPlayerResultElement = resultDivElement.firstElementChild
+   let secondPlayerResultElement = resultDivElement.lastElementChild
+   let firstTarget = null
+   let secondTarget = null
+   let historyElement = document.getElementById('history')
+
+   let firstHandler = function (e) {
+      if (!(e.target.src = "images/whiteCard.jpg")) {
+
+         e.target.src = "images/whiteCard.jpg"
+      }
+
+      firstPlayerResultElement.textContent = e.target.name
+      e.target.removeEventListener('click', firstHandler)
+      firstTarget = e.target
+      checkIfHandIsOver()
+   }
+
+   let secondHandler = function (e) {
+      if (!(e.target.src = "images/whiteCard.jpg")) {
+
+         e.target.src = "images/whiteCard.jpg"
+      }
+
+      secondPlayerResultElement.textContent = e.target.name
+      e.target.removeEventListener('click', secondHandler)
+      secondTarget = e.target
+      checkIfHandIsOver()
+   }
+
+   Array.from(firstPlayerCardsCollection.children).forEach(card => {
+      card.addEventListener('click', firstHandler)
+   })
+
+   Array.from(secondPlayerCardsCollection.children).forEach(card => {
+      card.addEventListener('click', secondHandler)
+   })
+
+   function checkIfHandIsOver() {
+      if (firstPlayerResultElement.textContent != '' && secondPlayerResultElement.textContent != '') {
+         if (Number(firstPlayerResultElement.textContent) > Number(secondPlayerResultElement.textContent)) {
+            firstTarget.style.border = '2px solid green'
+            secondTarget.style.border = '2px solid red'
+         } else {
+            firstTarget.style.border = '2px solid red'
+            secondTarget.style.border = '2px solid green'
+         }
+
+         firstTarget = null
+         secondTarget = null
+
+         historyElement.textContent += `[${firstPlayerResultElement.textContent} vs ${secondPlayerResultElement.textContent}] `
+         historyElement.textContent.trimEnd()
+
+         firstPlayerResultElement.textContent = ''
+         secondPlayerResultElement.textContent = ''
+      }
+   }
 }
