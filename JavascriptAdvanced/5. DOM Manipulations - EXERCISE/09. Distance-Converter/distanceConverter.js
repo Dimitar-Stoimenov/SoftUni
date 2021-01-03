70068786821b51035b742d962d01af3554bc1053
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-01-03 23:06:21 +0200

    added one more solution

--- JavascriptAdvanced/5. DOM Manipulations - EXERCISE/09. Distance-Converter/distanceConverter.js
+++ JavascriptAdvanced/5. DOM Manipulations - EXERCISE/09. Distance-Converter/distanceConverter.js
@@ -1,3 +1,70 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let btn = document.getElementById('convert')
+    let inputElement = document.getElementById('inputDistance')
+    let outputElement = document.getElementById('outputDistance')
+
+    let inputUnits = document.getElementById('inputUnits')
+    let outputUnits = document.getElementById('outputUnits')
+
+    btn.addEventListener('click', (e) => {
+        let inputNumber = Number(inputElement.value)
+        let convertedToMeters = 0
+        let result = 0
+
+        switch (inputUnits.value) {
+            case 'km':
+                convertedToMeters = inputNumber * 1000
+                break;
+            case 'm':
+                convertedToMeters = inputNumber
+                break;
+            case 'cm':
+                convertedToMeters = inputNumber / 100
+                break;
+            case 'mm':
+                convertedToMeters = inputNumber / 1000
+                break;
+            case 'mi':
+                convertedToMeters = inputNumber * 1609.34
+                break;
+            case 'yrd':
+                convertedToMeters = inputNumber * .9144
+                break;
+            case 'ft':
+                convertedToMeters = inputNumber * .3048
+                break;
+            case 'in':
+                convertedToMeters = inputNumber * .0254
+                break;
+        }
+
+        switch (outputUnits.value) {
+            case 'km':
+                result = convertedToMeters / 1000
+                break;
+            case 'm':
+                result = convertedToMeters
+                break;
+            case 'cm':
+                result = convertedToMeters * 100
+                break;
+            case 'mm':
+                result = convertedToMeters * 1000
+                break;
+            case 'mi':
+                result = convertedToMeters / 1609.34
+                break;
+            case 'yrd':
+                result = convertedToMeters / .9144
+                break;
+            case 'ft':
+                result = convertedToMeters / .3048
+                break;
+            case 'in':
+                result = convertedToMeters / .0254
+                break;
+        }
+
+        outputElement.value = result
+    })
 }
