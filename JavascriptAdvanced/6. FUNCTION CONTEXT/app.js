b8d7b089dae9ba8f9cf1fdf2a6659bff4c49f86b
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-01-09 00:54:25 +0200

    added one more solution

--- JavascriptAdvanced/6. FUNCTION CONTEXT/app.js
+++ JavascriptAdvanced/6. FUNCTION CONTEXT/app.js
@@ -1,3 +1,27 @@
 function solve() {
-    // ToDo
+    let dropdownBtn = document.getElementById('dropdown')
+    let dropdownElement = document.getElementById('dropdown-ul')
+    let resultBox = document.getElementById('box')
+
+    dropdownBtn.addEventListener('click', (e) => {
+        if (dropdownElement.style.display == 'block') {
+            dropdownElement.style.display = 'none'
+            resultBox.style.color = 'white'
+            resultBox.style.backgroundColor = 'black'
+        } else {
+            dropdownElement.style.display = 'block'
+        }
+    })
+
+    Array.from(dropdownElement.children).forEach(box => {
+        box.addEventListener('click', (e) => {
+            let rgb = e.currentTarget.textContent
+            changeBackgroundOfBox.call(resultBox, rgb)
+        })
+    })
+
+    function changeBackgroundOfBox(rgb) {
+        this.style.backgroundColor = rgb
+        this.style.color = 'black'
+    }
 }
