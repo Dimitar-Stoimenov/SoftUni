76308a2d735293fa7faac5a0f60e956cc977ae03
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-03-17 00:52:03 +0200

    added more solutions

--- JavascriptAdvanced/10. UNIT TESTING AND ERROR HANDLING - EXERCISE/06.Dynamic-Validation/app.js
+++ JavascriptAdvanced/10. UNIT TESTING AND ERROR HANDLING - EXERCISE/06.Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    // TODO
+    let emailInputElement = document.getElementById('email');
+
+    let pattern = /[a-z]+@[a-z]+.[a-z]+/g
+
+    emailInputElement.addEventListener('change', (e) => {
+        if (!pattern.test(emailInputElement.value)) {
+            emailInputElement.classList.add('error');
+        } else {
+            emailInputElement.removeAttribute('class');
+        }
+    })
 }
--- JavascriptAdvanced/10. UNIT TESTING AND ERROR HANDLING - EXERCISE/07.Form-Validation/formValidation.js
+++ JavascriptAdvanced/10. UNIT TESTING AND ERROR HANDLING - EXERCISE/07.Form-Validation/formValidation.js
@@ -1,3 +1,41 @@
 function validate() {
-    // TODO
+    let usernameElement = document.getElementById('username');
+    let emailElement = document.getElementById('email');
+    let passwordElement = document.getElementById('password');
+    let confirmPasswordElement = document.getElementById('confirm-password');
+    let companyElement = document.getElementById('company');
+    let companyInfoElement = document.getElementById('companyInfo');
+
+    let companyNumberElement = document.getElementById('companyNumber')
+
+    companyElement.addEventListener('click', (e) => {
+        if (companyElement.checked) {
+            companyInfoElement.style.display = 'block';
+        } else {
+            companyInfoElement.style.display = 'none';
+        }
+    });
+
+    let submitButtonElement = document.getElementById('submit');
+
+    submitButtonElement.addEventListener('click', (e) => {
+        if (companyElement.checked) {
+            let companyNumberPattern = /^[1-9][0-9]{3}$/g
+
+            if (!companyNumberPattern.test(companyNumberElement.value)) {
+                companyNumberElement.style.borderColor = 'red';
+            } else {
+                companyNumberElement.style.border = 'none';
+
+            }
+        }
+
+        let userPattern = /[a-z0-9]{3,20}/g
+
+        if (!userPattern.test(usernameElement.value)) {
+            usernameElement.style.borderColor = 'red';
+        } else {
+            usernameElement.style.border = 'none';
+        }
+    })
 }
