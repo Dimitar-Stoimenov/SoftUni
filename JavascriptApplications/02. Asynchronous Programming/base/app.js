d2c72d97c0ef189166469d2aed715fa19c072d3b
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-05-07 00:04:44 +0300

    added a solution

--- JavascriptApplications/02. Asynchronous Programming/base/app.js
+++ JavascriptApplications/02. Asynchronous Programming/base/app.js
@@ -5,72 +5,60 @@ async function getRecipeList() {
 
     try {
         const response = await fetch(url);
+
+        if (response.ok == false) {
+            throw new Error(response.statusText);
+        }
+
         const recipes = await response.json();
         mainElement.innerHTML = '';
 
-        Object.values(recipes).forEach(r => {
-            let recipeId = r._id;
-
-            let recipeName = r.name;
-            let img = r.img;
-
-            let newRecipeElement = document.createElement('article');
-            newRecipeElement.classList = 'preview';
-
-            let divTitle = document.createElement('div');
-            divTitle.classList = 'title';
-            let h2titleElement = document.createElement('h2');
-            h2titleElement.textContent = recipeName;
-            divTitle.appendChild(h2titleElement);
-
-            newRecipeElement.appendChild(divTitle);
-
-            let divImg = document.createElement('div');
-            divImg.classList = 'small';
-            divImg.innerHTML = `<img src=${img}></img>`;
-
-            newRecipeElement.appendChild(divImg);
-
-            mainElement.appendChild(newRecipeElement);
-
-            newRecipeElement.addEventListener('click', (event) => {
-                let context = event.currentTarget;
-                let data = recipeData(recipeId, context);
-                
-            });
-        });
+        Object.values(recipes).map(createPreview).forEach(r => mainElement.appendChild(r));
 
     } catch (error) {
         alert(error.message);
     }
 }
 
-window.addEventListener('load', () => {
-    getRecipeList();
-});
+function createPreview(recipe) {
+    let recipeId = recipe._id;
+    let recipeName = recipe.name;
+    let img = recipe.img;
 
-async function recipeData(recipeId, context) {
-    const recipeUrl = `http://localhost:3030/jsonstore/cookbook/details/${recipeId}`;
+    let result = document.createElement('article');
+    result.classList = 'preview';
+
+    let divTitle = document.createElement('div');
+    divTitle.classList = 'title';
+    let h2titleElement = document.createElement('h2');
+    h2titleElement.textContent = recipeName;
+    divTitle.appendChild(h2titleElement);
+
+    result.appendChild(divTitle);
+
+    let divImg = document.createElement('div');
+    divImg.classList = 'small';
+    divImg.innerHTML = `<img src=${img}></img>`;
+    result.appendChild(divImg);
+
+    result.addEventListener('click', () => getRecipeDetails(recipeId, result));
+
+    return result;
+}
+
+async function getRecipeDetails(id, preview) {
+    const recipeUrl = `http://localhost:3030/jsonstore/cookbook/details/` + id;
+
     const recipeResponse = await fetch(recipeUrl);
-    let data = await recipeResponse.json();
+    const data = await recipeResponse.json();
 
-    console.log(context);
-    console.log(data);
+    const article = document.createElement('article');
     
-    context.classList = '';
-    context.children[0].remove();
-    context.children[0].remove();
-
-    let ingredientsArr = data.ingredients;
-    let stepsArr = data.steps;
-
     const titleh2Element = document.createElement('h2');
     titleh2Element.textContent = data.name;
-    context.appendChild(titleh2Element);
 
     const bandElement = document.createElement('div');
     bandElement.classList = 'band';
-    context.appendChild(bandElement);
 
     const thumbElement = document.createElement('div');
     thumbElement.classList = 'thumb';
@@ -84,11 +72,35 @@ async function recipeData(recipeId, context) {
     const ingredientsh3 = document.createElement('h3');
     ingredientsh3.textContent = "Ingredients:";
     ingredientsElement.appendChild(ingredientsh3);
-
-
-
+    const ulIngredientElement = document.createElement('ul');
+    data.ingredients.map(i => {
+        let liIngredientElement = document.createElement('li');
+        liIngredientElement.textContent = i;
+        ulIngredientElement.appendChild(liIngredientElement);
+    });
+    ingredientsElement.appendChild(ulIngredientElement);
+    bandElement.appendChild(ingredientsElement);
 
     const descriptionElement = document.createElement('div');
     descriptionElement.classList = 'description';
-    context.appendChild(descriptionElement);
+
+    const descTitleh2Element = document.createElement('h2');
+    descTitleh2Element.textContent = 'Preparation:';
+    descriptionElement.appendChild(descTitleh2Element);
+
+    data.steps.map(s => {
+        let stepParagraphElement = document.createElement('p');
+        stepParagraphElement.textContent = s;
+        descriptionElement.appendChild(stepParagraphElement);
+    })
+
+    article.appendChild(titleh2Element);
+    article.appendChild(bandElement);
+    article.appendChild(descriptionElement);
+
+    preview.replaceWith(article);
 }
+
+window.addEventListener('load', () => {
+    getRecipeList();
+});
