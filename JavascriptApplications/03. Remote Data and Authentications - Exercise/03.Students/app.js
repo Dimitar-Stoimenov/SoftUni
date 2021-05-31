76c088509046879e488914321651c63aad52c0c3
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-05-31 23:44:43 +0300

    update

--- JavascriptApplications/03. Remote Data and Authentications - Exercise/03.Students/app.js
+++ JavascriptApplications/03. Remote Data and Authentications - Exercise/03.Students/app.js
@@ -1,1 +1,55 @@
-console.log('TODO...');
+function start() {
+    displayStudents();
+
+    document.getElementById('submit').addEventListener('click', createStudent);
+}
+
+start();
+
+async function getStudentsList() {
+    const url = 'http://localhost:3030/jsonstore/collections/students';
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+}
+
+async function displayStudents() {
+    const data = await getStudentsList();
+    const tableBodyEl = document.querySelector('#results > tbody');
+    const rows = Object.values(data).map(createRow).join('');
+
+    function createRow(s) {
+        const result =
+            `<tr>
+            <td>${s.firstName}</td>
+            <td>${s.lastName}</td>
+            <td>${s.facultyNumber}</td>
+            <td>${s.grade}</td>
+            </tr>`
+
+        return result;
+    }
+
+    tableBodyEl.innerHTML = rows;
+}
+
+async function createStudent(event) {
+    event.preventDefault();
+
+    const formEl = document.getElementById('form');
+    const formData = new FormData(formEl);
+    const firstName = formData.get('firstName');
+    const lastName = formData.get('lastName');
+    const facultyNumber = formData.get('facultyNumber');
+    const grade = formData.get('grade');
+
+    const url = 'http://localhost:3030/jsonstore/collections/students';
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'json/application' },
+        body: JSON.stringify({ firstName, lastName, facultyNumber, grade })
+    });
+
+    displayStudents();
+}
